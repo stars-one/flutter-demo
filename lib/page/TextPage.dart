f79970b4a626d5d8b75d97552d5d4b54f12e152d
stars-one <1053894518@qq.com>
2020-10-19 14:21:36 +0800

    新增文本编辑控件的相关操作demo

--- lib/page/TextPage.dart
+++ lib/page/TextPage.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 
 import 'MyMaterialPage.dart';
 
-class TextPage extends StatelessWidget{
+class TextPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var detailAddressController = TextEditingController();
+
     return MyMaterialPage(
         "文本演示",
         Column(
@@ -12,7 +14,7 @@ class TextPage extends StatelessWidget{
             Container(
               padding: EdgeInsets.all(10),
               child: TextField(
-                  //controller: detailAddressController,
+                  controller: detailAddressController,
                   keyboardType: TextInputType.multiline,
                   maxLines: 5,
                   minLines: 3,
@@ -21,8 +23,25 @@ class TextPage extends StatelessWidget{
                       hintText: '详细地址',
                       hintStyle: TextStyle(fontSize: 14))),
             ),
+            FlatButton(
+              onPressed: () {
+                print(detailAddressController.text.toString());
+              },
+              child: Text("获取输入框内容"),
+            ),
+            FlatButton(
+              onPressed: () {
+                detailAddressController.clear();
+              },
+              child: Text("清空内容"),
+            ),
+            FlatButton(
+              onPressed: () {
+                detailAddressController.text = "hello";
+              },
+              child: Text("修改内容为hello"),
+            ),
           ],
         ));
   }
-
 }
